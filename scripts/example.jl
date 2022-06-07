6348122bdc4d89b3164db6b713654032ee23813e
msmerlak <smerlak@mis.mpg.de>
2022-06-07 17:03:15 +0200

    typo

--- scripts/example.jl
+++ scripts/example.jl
@@ -1,13 +1,14 @@
 using Distributed
 addprocs(5)
 
+
 @everywhere using DrWatson
 @everywhere @quickactivate
 @everywhere using RicciCurvatures
 
 using Graphs
 
-g = SimpleGraph(4);
+g = SimpleGraph(4)
 add_edge!(g, 1, 2)
 add_edge!(g, 2, 3)
 add_edge!(g, 3, 4)
